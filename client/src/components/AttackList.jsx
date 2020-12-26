7f1da9a4d1b9727b223f835217a398825d13b93a
Xin Ying <e0323888@u.nus.edu>
2020-12-26 21:55:07 +0800

    Attack done except handle submit

--- client/src/components/AttackList.jsx
+++ client/src/components/AttackList.jsx
@@ -5,65 +5,94 @@ class AttackList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            attackingPuppyId: -1,
-            targetPuppyId: -1,
-            message: ""
+            attackingPuppyId: 0,
+            targetPuppyId: 0,
+            message: "",
+            myPuppies: [],
+            otherPuppies: []
         }
         this.handleInputChange = this.handleInputChange.bind(this)
 
+    }
+
+    componentDidMount() {
+        const getPuppies = async() => {
+            const contract = this.props.contract;
+            const accounts = this.props.accounts;
+            var myPuppies = [];
+            var otherPuppies = [];
+            const length = await contract.methods.getAllPuppiesNumber().call()
+            for (var i = 0; i < length; i++) {
+                const puppy = await contract.methods.allPuppies(i).call()
+                if (puppy.owner === accounts[0] && puppy.level > 0) {
+                    myPuppies.push(puppy);
+                } else if (puppy.owner !== accounts[0] && puppy.level > 0) {
+                    otherPuppies.push(puppy);
+                }
+            }
+
+            var myPuppiesList = '';
+            var otherPuppiesList = '';
+            
+            myPuppiesList = myPuppies.map(puppy => (
+                <option key= {puppy.id} value={puppy.id}>Name: {puppy.name} Level: {puppy.level}</option>
+            ))        
+            otherPuppiesList = otherPuppies.map(puppy => (
+                <option key= {puppy.id} value={puppy.id}>Name: {puppy.name} Level: {puppy.level}</option>
+            ))
+            
+            this.setState({
+                myPuppiesList: myPuppiesList,
+                otherPuppiesList: otherPuppiesList
+            })
+        }
+        
+        getPuppies()
     }
 
     handleInputChange(event) {
         const target = event.target
         const value = target.value
         const name = target.name
-
         this.setState({
             [name]: value
         })
     }
 
-    async handleSubmit() {
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("H")
+        console.log(this.state.attackingPuppyId)
+        console.log(this.state.targetPuppyId)
         const attackingPuppyId = this.state.attackingPuppyId
         const targetPuppyId = this.state.targetPuppyId
-        if (attackingPuppyId === -1 || targetPuppyId === -1) {
+        
+        this.props.contract.methods._attack(attackingPuppyId, targetPuppyId).send({ from: this.props.accounts[0] })
+        .on("receipt", (receipt) => {
+            alert("Success")
+        })
+        .on("error", error => {
+            alert(error.message);
+        });
+    
+        /*this.props.contract.events.SuccessAttack({ filter: { _attackingPuppyId: attackingPuppyId } })
+            .on("data", function(event) {
             this.setState({
-                message: "Choose a attacking Puppy and a target Puppy please"
-            })
-        } else {
-            await this.props.contract.methods._attack(attackingPuppyId, targetPuppyId)
-            //DElete the need for return parameters. Or only one event with message passed as success or failure
-            await this.props.contract.events.SuccessAttack({ filter: { _attackingPuppyId: attackingPuppyId } })
-                .on("data", function(event) {
-                this.setState({
-                    message: "Attack Success! Such a brilliant puppy! Your puppy's level have increased by one :)"
-                })}).on("error", console.error);
+                message: "Attack Success! Such a brilliant puppy! Your puppy's level have increased by one :)"
+            })}).on("error", console.error);
 
-            await this.props.contract.events.FailAttack({ filter: { _attackingPuppyId: attackingPuppyId } })
-                .on("data", function(event) {
-                this.setState({
-                    message: "Attack Failed! No penalties given to your puppy. Better luck next time! :)"
-                })}).on("error", console.error);
-        }
+        this.props.contract.events.FailAttack({ filter: { _attackingPuppyId: attackingPuppyId } })
+            .on("data", function(event) {
+            this.setState({
+                message: "Attack Failed! No penalties given to your puppy. Better luck next time! :)"
+            })}).on("error", console.error);*/
+    
     }
     
-    render() {
-        console.log(this.props.contract.methods.allPuppies(0).call().then(function(result) {console.log(result)}))
-        console.log(this.props.contract.methods.getAllPuppiesNumber().call())
-        console.log(this.props.contract.methods.getMyPuppiesName(0).call())
-        console.log(this.props.contract.methods.getMyPuppiesLevel(0).call())
-        const myPuppies = this.props.contract.methods.userToPuppy(this.props.accounts).call()
-        console.log(myPuppies)
-        const allPuppies = this.props.contract.methods.allPuppies(0).call().then(x => x.filter(y => y.owner !== this.props.accounts))
-        
-        const myPuppiesList = myPuppies.map(puppy => (
-            <option value={puppy.id}>Name: {puppy.name} Level: {puppy.level}</option>
-        ))        
-        const otherPuppiesList = allPuppies.map(puppy => (
-            <option value={puppy.id}>Name: {puppy.name} Level: {puppy.level}</option>
-        ))
 
-        var message = '';
+    render() {
+
+        /*var message = '';
         const [show, setShow] = useState(true);
         if (this.state.message !== "") {
             message = (
@@ -71,31 +100,37 @@ class AttackList extends React.Component {
                     {this.state.message}
                 </AlertMessage>
             )
-        }
+        }*/
+        var myPuppiesList = this.state.myPuppiesList
+        var otherPuppiesList = this.state.otherPuppiesList
 
-        return (
+
+        return(
             <div>
                 <h3>Attack Another Puppy!!!</h3>
-                <form name="AttackForm" onSubmit={this.handleSubmit()}>
-                    <div class="form-group">
-                        <label for="myPuppies">Select your puppy to attack:</label>
-                        <select class="form-control" id="myPuppies" value={this.state.attackingPuppyId} onChange={this.handleInputChange} required>
+                <h5>If you see error messages, please ensure that you possess puppies with levels > 0 and there are eligible puppies for you to attack</h5>
+                <form name="AttackForm" onSubmit={this.handleSubmit}>
+                    <div className="row">
+                    <div className="form-group col-md-4">
+                        <label htmlFor="myPuppies">Select your puppy to attack:</label>
+                        <select className="form-control" id="myPuppies" name="attackingPuppyId" value={this.state.attackingPuppyId} onChange={this.handleInputChange} required>
                             {myPuppiesList}
                         </select>
                     </div>
 
-                    <div class="form-group">
-                        <label for="otherPuppies">Select another puppy to attack:</label>
-                        <select class="form-control" id="otherPuppies" value={this.state.targetPuppyId} onChange={this.handleInputChange} required>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="otherPuppies">Select another puppy to attack:</label>
+                        <select className="form-control" id="otherPuppies" name="targetPuppyId" value={this.state.targetPuppyId} onChange={this.handleInputChange} required>
                             {otherPuppiesList}
                         </select>
                     </div>
-                    <input id="submit" type="submit" value="Attack!!!"></input>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
                 </form>
-
-                {message}
             </div>
         )
+            
+        
     }
 }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import AlertMessage from '../components/AlertMessage';
 import ScrollAble from '../components/ScrollAble';
 import SearchBox from '../components/SearchBox';
 import 'tachyons'
-// import AttackList from '../components/AttackList';
+import AttackList from '../components/AttackList';
 import BuyPuppy from '../components/BuyPuppy';
 
 
@@ -20,9 +20,7 @@ const HomePage = props => {
     useEffect(() => {
         setShow(true);
     }, []); 
-
-    console.log(searchfield);
-
+    
     return (
         <div>
             <div className="tc">
@@ -32,7 +30,7 @@ const HomePage = props => {
                 <h1 className="f2">Puppies Attack</h1>
                 <SearchBox searchChange = {onSearchChange} />
                 <BuyPuppy web3={props.web3} contract={props.contract} accounts={props.accounts}/>
-                {/* <AttackList web3={props.web3} contract={props.contract} accounts={props.accounts}></AttackList> */}
+                <AttackList web3={props.web3} contract={props.contract} accounts={props.accounts}></AttackList>
                 <ScrollAble>
                     {/* <Cardlist cats={filteredCats} /> */}
                 </ScrollAble>
