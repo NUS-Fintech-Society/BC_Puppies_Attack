fcc6cc097e1227246e2682bb25283cb2bf4b61ef
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2020-12-26 22:11:56 +0800

    Add change name function

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import SearchBox from '../components/SearchBox';
 import 'tachyons'
 // import AttackList from '../components/AttackList';
 import BuyPuppy from '../components/BuyPuppy';
+import ChangeName from '../components/ChangeName';
 
 
 const HomePage = props => {
@@ -32,6 +33,7 @@ const HomePage = props => {
                 <h1 className="f2">Puppies Attack</h1>
                 <SearchBox searchChange = {onSearchChange} />
                 <BuyPuppy web3={props.web3} contract={props.contract} accounts={props.accounts}/>
+                <ChangeName web3={props.web3} contract={props.contract} accounts={props.accounts}/>
                 {/* <AttackList web3={props.web3} contract={props.contract} accounts={props.accounts}></AttackList> */}
                 <ScrollAble>
                     {/* <Cardlist cats={filteredCats} /> */}
